fa89ca5f59bcf39eb2878b2dadb473a35f417186
Kbotei <eclipcenet@gmail.com>
2025-08-12 16:18:54 -0500

    updates to applicant details form

---
+++ ApplicationContractingBlazorApp/Components/Application/ApplicantModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationContractingBlazorApp.Components.Application;
+
+public class ApplicantModel
+{
+    [Required(AllowEmptyStrings = false)]
+    public string FirstName { get; set; } = "";
+
+    [Required(AllowEmptyStrings = false)]
+    public string LastName { get; set; } = "";
+
+    [Required(AllowEmptyStrings = false)]
+    public string Email { get; set; } = "";
+
+    [Required(AllowEmptyStrings = false)]
+    public string PhoneNumber { get; set; } = "";
+
+    [Required(AllowEmptyStrings = false)]
+    public string StreetAddress { get; set; } = "";
+
+    [Required(AllowEmptyStrings = false)]
+    public string City { get; set; } = "";
+
+    [Required(AllowEmptyStrings = false)]
+    public string? State { get; set; }
+
+    [Required(AllowEmptyStrings = false)]
+    [MaxLength(5)]
+    public string ZipCode { get; set; } = "";
+}
